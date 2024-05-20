b557fdc5a270331ec06fb1010d754a72b474e9b6
syihwamoza <mozakastella26@gmail.com>
2024-05-20 10:41:18 +0700

    memasukkan int main

--- member_list/member_list.cpp
+++ member_list/member_list.cpp
@@ -9,7 +9,19 @@ private:
 	float nilai;
 
 public:
-	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama)
-
+	mahasiswa(int pId, string pNama, float pNilai):id(pId),nama(pNama), nilai (pNilai){
     }
+
+   ~mahasiswa() {
+	cout << "Id      = " << id << endl;
+	cout << "Nama    = " << nama << endl;
+	cout << "Nilai   = " << nilai << endl;
+   }
+};
+
+int main() {
+	mahasiswa mhs(12, "Asroni", 90.5);
+
+	return 0;
+}
    
