36d15755305de72fbad697a49f4c8e6ab572f5e7
syihwamoza <mozakastella26@gmail.com>
2024-05-20 11:43:40 +0700

    membuat objek dari kelas ibu

--- MainA/MainA.cpp
+++ MainA/MainA.cpp
@@ -5,6 +5,20 @@ using namespace std;
 #include "ibu.h"
 
 int main() {
+	ibu* varIbu = new ibu("dini");
+	ibu* varIbu2 = new ibu("novi");
+	anak* varAnak1 = new ("tono");
+	anak* varAnak2 = new ("rini");
+	anak* varAnak3 = new ("Dewi");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varIbu2->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak1);
+
+	varIbu->cetakAnak();
+	varibu2->cetakAnak();
+
 
 }
 
